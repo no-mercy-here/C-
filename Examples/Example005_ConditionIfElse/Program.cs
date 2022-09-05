281660d8518233cc0fd7ffb77010d977966c3a77
Anastasiya Strakh <nastya.strakh@gmail.com>
2022-09-05 16:25:29 +0300

    Example 005 added

---
+++ Examples/Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha") {
+    Console.WriteLine("Hello, Masha. How are doing today?");
+} 
+
+else {
+    Console.WriteLine("Hello");
+    Console.WriteLine(username);
+}
